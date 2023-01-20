739a7a0bd7f6a4291fd3b144192e163b53b53d33
Corbin Broadhead <poujmail@gmail.com>
2023-01-19 19:38:10 -0700

    Fin.

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,5 +1,13 @@
-let x = 1
-let y = 2
+let x = 37
+let y = 41
+
+if (x > y) {
+    console.log(x);
+} else if (y > x) {
+    console.log(y);
+} else if (x === y) {
+    console.log("The values are identical.");
+}
 
 let latestScore = "Scorpions 41, Levies 0";
 
